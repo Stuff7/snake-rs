450233032fcd0f60de36ac41198dc9f61aa32360
Armando <cuentafrst@gmail.com>
2022-07-11 07:42:03 -0500

    separate game physics from framerate

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,37 @@ use std::{rc::Rc, cell::RefCell};
 use snake::Snake;
 
 fn main() {
-  let game = SnakeGame {
-    snake: Snake::new(6, 10),
-  };
-  termren::Renderer::new(Rc::new(RefCell::new(game))).set_fps(30).run();
+  let game = SnakeGame::new(Snake::new(6, 10));
+  termren::Renderer::new(Rc::new(RefCell::new(game))).set_fps(60).run();
 }
+
+const TPS: f64 = 1.0 / 12.0;
 
 struct SnakeGame {
   snake: Snake,
+  timedelta_passed: f64,
+}
+
+impl SnakeGame {
+  pub fn new(snake: Snake) -> Self {
+    Self { snake, timedelta_passed: 0.0 }
+  }
 }
 
 impl termren::EventHandler for SnakeGame {
   fn update(
     &mut self,
-    event_option: Option<termren::Event>
+    event_option: Option<termren::Event>,
+    timedelta: f64,
   ) -> termren::SingleOrMulti<termren::Pixel> {
+    self.timedelta_passed = self.timedelta_passed + timedelta;
     if let Some(event) = event_option {
       self.snake.steer(event);
     }
-
-    self.snake.serpentine();
+    if self.timedelta_passed >= TPS {
+      self.timedelta_passed = 0.0;
+      self.snake.serpentine();
+    }
 
     termren::SingleOrMulti::Multi(&self.snake.body)
   }
