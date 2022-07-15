9a53401da2f25b72a90a3a9e402f630d988ff815
Armando <cuentafrst@gmail.com>
2022-07-14 22:22:38 -0500

    add game over logic

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,17 @@ use food::Food;
 
 fn main() {
   let game = SnakeGame::new(Snake::new(6, 10, 10));
-  // let game = RainbowMosaicGame::new();
   termren::Renderer::new(Rc::new(RefCell::new(game))).set_fps(60).run();
 }
 
 const TPS: f64 = 1.0 / 30.0;
 
+const GREEN: &str = "\x1b[38;2;100;200;50m";
+const RED: &str = "\x1b[38;2;200;100;50m";
+
 struct SnakeGame {
   snake: Snake,
+  snake_clone: Snake,
   food: Food,
   pixels: Vec<termren::Pixel>,
   timedelta_passed: f64,
@@ -24,10 +27,20 @@ struct SnakeGame {
 impl SnakeGame {
   pub fn new(snake: Snake) -> Self {
     Self {
+      snake_clone: snake.clone(),
       snake,
       food: Food::new(20, 10),
       pixels: vec![],
       timedelta_passed: 0.0,
+    }
+  }
+
+  pub fn handle_input(&mut self, key_event: &termren::KeyEvent) {
+    self.snake.steer(key_event);
+    if !self.snake.alive {
+      if key_event.code == termren::KeyCode::Char('r') {
+        self.snake = self.snake_clone.clone();
+      }
     }
   }
 }
@@ -38,13 +51,15 @@ impl termren::EventHandler for SnakeGame {
     ctx: &termren::Context,
   ) -> (termren::Group<termren::Pixel>, Option<String>) {
     self.timedelta_passed = self.timedelta_passed + ctx.timedelta;
-    if let Some(event) = ctx.event {
-      self.snake.steer(event);
+
+    if let Some(key_event) = termren::event::to_key_event(ctx.event) {
+      self.handle_input(&key_event);
     }
+
     if self.timedelta_passed >= TPS {
       self.timedelta_passed = 0.0;
       self.snake.try_eat(&mut self.food, ctx);
-      self.snake.serpentine();
+      self.snake.serpentine(ctx);
     }
 
     self.pixels = vec![
@@ -55,47 +70,28 @@ impl termren::EventHandler for SnakeGame {
 
     (
       termren::Group::Multi(self.pixels.as_slice()),
-      Some(format!(
-        "{pos_start}Score: {green}{snake_length}{reset} \
-        Food: {green}({food_x}, {food_y}){reset} \
-        Snake: {green}({snake_x}, {snake_y}){reset} \
-        Console: {green}({csl_x}, {csl_y}){reset}",
-        pos_start = termren::console::seq::CURSOR_START,
-        green = termren::console::seq::fg_rgb(100, 200, 50),
-        snake_length = self.snake.len(),
-        reset = termren::console::seq::RESET,
-        food_x = self.food.pixel.x,
-        food_y = self.food.pixel.y,
-        snake_x = self.snake.head.x,
-        snake_y = self.snake.head.y,
-        csl_x = ctx.console_size.width,
-        csl_y = ctx.console_size.height,
-      ))
+      {
+        let mut ui = format!(
+          "{pos_start}Score: {GREEN}{score}{reset} \
+          Food: {GREEN}({food_x}, {food_y}){reset} \
+          Snake: {GREEN}({snake_x}, {snake_y}){reset}",
+          pos_start = termren::console::seq::CURSOR_START,
+          score = self.snake.len(),
+          reset = termren::console::seq::RESET,
+          food_x = self.food.pixel.x,
+          food_y = self.food.pixel.y,
+          snake_x = self.snake.head.x,
+          snake_y = self.snake.head.y,
+        );
+        if !self.snake.alive {
+          ui.push_str(&format!(
+            " {bold}{RED}Game Over!{reset} Press {GREEN}R{reset} to play again",
+            bold = termren::console::seq::BOLD,
+            reset = termren::console::seq::RESET,
+          ))
+        }
+        Some(ui)
+      }
     )
   }
 }
-
-// struct RainbowMosaicGame {
-//   pixels: Vec<termren::Pixel>,
-// }
-
-// impl RainbowMosaicGame {
-//   pub fn new() -> Self {
-//     let mut pixels = vec![];
-//     for x in 1..120 {
-//       for y in 1..30 {
-//         pixels.push((x, y).into());
-//       }
-//     }
-//     Self { pixels }
-//   }
-// }
-
-// impl termren::EventHandler for RainbowMosaicGame {
-//   fn update(
-//     &mut self,
-//     _: &termren::Context,
-//   ) -> (termren::Group<termren::Pixel>, Option<String>) {
-//     (termren::Group::Multi(self.pixels.as_slice()), None)
-//   }
-// }
--- src/snake.rs
+++ src/snake.rs
@@ -4,7 +4,9 @@ use termren::Pixel;
 const SNAKE_COLOR: termren::Color = termren::Color { r: 120, g: 220, b: 120 };
 const SNAKE_PX: &str = "░░";
 
+#[derive(Clone)]
 pub struct Snake {
+  pub alive: bool,
   pub head: Pixel,
   pub body: VecDeque<Pixel>,
   direction: Direction,
@@ -19,6 +21,7 @@ impl Snake {
       body.push((x, y, SNAKE_COLOR, SNAKE_PX).into());
     }
     Self {
+      alive: true,
       head: (x, y, SNAKE_COLOR, SNAKE_PX).into(),
       body: body.into(),
       direction: Direction::Right,
@@ -31,47 +34,50 @@ impl Snake {
     self.body.len()
   }
 
-  fn simulate_serpentine(&mut self) -> Pixel {
+  fn try_move(&mut self, ctx: &termren::Context) -> Pixel {
     if self.prev_direction == self.direction && self.next_direction.is_some() {
       self.direction = std::mem::replace(&mut self.next_direction, None).unwrap();
     }
     self.prev_direction = self.direction;
-    match self.direction {
+    let next_head: Pixel = match self.direction {
       Direction::Up => (self.head.x, self.head.y - 1, SNAKE_COLOR, SNAKE_PX).into(),
       Direction::Right => (self.head.x + 2, self.head.y, SNAKE_COLOR, SNAKE_PX).into(),
       Direction::Down => (self.head.x, self.head.y + 1, SNAKE_COLOR, SNAKE_PX).into(),
       Direction::Left => (self.head.x - 2, self.head.y, SNAKE_COLOR, SNAKE_PX).into(),
+    };
+    self.alive = !(
+      next_head.x <= 0 ||
+      next_head.x > ctx.console_size.width ||
+      next_head.y <= 0 ||
+      next_head.y > ctx.console_size.height
+    ) && !self.body.iter().any(|part| next_head == *part);
+
+    next_head
+  }
+
+  pub fn steer(&mut self, key_event: &termren::KeyEvent) {
+    if !self.alive {
+      return;
+    }
+    if let Some(dir) = Direction::from_key_code(key_event.code) {
+      if self.direction != self.prev_direction && dir.inverse() != self.direction {
+        self.next_direction = Some(dir);
+      } else if dir.inverse() != self.prev_direction {
+        self.direction = dir;
+      }
     }
   }
 
-  pub fn steer(&mut self, event: termren::Event) {
-    if let termren::Event::Key(key_event) = event {
-      if let Some(dir) = Direction::from_event(key_event) {
-        if self.direction != self.prev_direction && dir.inverse() != self.direction {
-          self.next_direction = Some(dir);
-        } else if dir.inverse() != self.prev_direction {
-          self.direction = dir;
-        }
-      }
-
-      if key_event.code == termren::KeyCode::Char('e') {
-        let next_head = self.simulate_serpentine();
-        self.body.push_front(std::mem::replace(&mut self.head, next_head));
-      }
-      if key_event.code == termren::KeyCode::Char('q') {
-        self.body.pop_back();
-      }
+  pub fn serpentine(&mut self, ctx: &termren::Context) {
+    if self.alive {
+      let next_head = self.try_move(ctx);
+      self.body.push_front(std::mem::replace(&mut self.head, next_head));
+      self.body.pop_back();
     }
-  }
-
-  pub fn serpentine(&mut self) {
-    let next_head = self.simulate_serpentine();
-    self.body.push_front(std::mem::replace(&mut self.head, next_head));
-    self.body.pop_back();
   }
 
   pub fn try_eat(&mut self, food: &mut super::Food, ctx: &termren::Context) {
-    if self.head == food.pixel {
+    if self.alive && self.head == food.pixel {
       if let Some(last_tail) = self.body.back() {
         self.body.push_back(*last_tail);
       } else {
@@ -100,8 +106,8 @@ impl Direction {
     }
   }
 
-  pub fn from_event(key_event: termren::KeyEvent) -> Option<Direction> {
-    match key_event.code {
+  pub fn from_key_code(key_code: termren::KeyCode) -> Option<Direction> {
+    match key_code {
       termren::KeyCode::Up => Some(Direction::Up),
       termren::KeyCode::Right => Some(Direction::Right),
       termren::KeyCode::Down => Some(Direction::Down),
